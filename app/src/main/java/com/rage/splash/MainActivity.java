207a9006f6d039eff7677bfddee7afa22c57bb45
Aaahh Ahh <woohoomoo2u@gmail.com>
2020-02-01 19:17:28 -0500

    ok lets get this bread

--- app/src/main/java/com/rage/splash/MainActivity.java
+++ app/src/main/java/com/rage/splash/MainActivity.java
@@ -1,6 +1,7 @@
 package com.rage.splash;
 
 import androidx.appcompat.app.AppCompatActivity;
+import io.radar.sdk.Radar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Radar.initialize(publishableKey);
     }
 }
